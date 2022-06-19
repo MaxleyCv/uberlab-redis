3322ac12a48f2337144d03ad65a0f8c55b4535a4
maxleszcz <maksym@neuroflow.com>
2022-06-19 15:36:42 +0200

    Added settings

---
+++ config/settings.py
@@ -0,0 +1,16 @@
+import os
+
+from utils.metaclasses import Singleton
+
+
+class Config(metaclass=Singleton):
+
+    def update_settings(self):
+        settings_dict = dict(
+            REDIS_HOST=os.environ.get('REDIS_HOST'),
+            REDIS_PASSWORD=os.environ.get('REDIS_PASSWORD'),
+            USE_CLOUD_EXTENSION=os.environ.get('USE_CLOUD_EXTENSION'),
+            REDIS_PORT=os.environ.get('REDIS_PORT'),
+            READING_URL=os.environ.get('READING_URL')
+        )
+        return settings_dict
